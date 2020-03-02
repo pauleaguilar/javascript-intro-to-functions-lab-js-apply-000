5e1fb03a4503a21455c2fd915a2b11d7dd8127b4
Paul Aguilar <pauleaguilar@gmail.com>
2020-03-02 02:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function whisper(string) {
 function logShout(string) {
   console.log(shout(string))
 }
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase()
+      return "I can't hear you"
+  if string === string.toUpperCase()
+      return "YES INDEED"
+  if string === "I love you"
+      return "I love you, too"
+}
